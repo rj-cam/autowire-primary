b489eda78f57125d01564d0cc5449bcc00a41d93
Rj <rj.camarillo@tagitmobile.com>
2022-04-20 18:19:38 +0800

    Clean-up tests

--- src/test/java/com/example/autowireprimary/AutowirePrimaryApplicationWorkingTests.java
+++ src/test/java/com/example/autowireprimary/AutowirePrimaryApplicationWorkingTests.java
@@ -11,36 +11,27 @@ import org.springframework.beans.factory.support.BeanDefinitionRegistry;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.ApplicationContext;
 
-import com.example1.SomeHostDASImpl1;
 import com.example2.SomeHostDASImpl2;
 
 import lombok.extern.log4j.Log4j2;
 
 @SpringBootTest
 @Log4j2
-class AutowirePrimaryApplicationTests {
+class AutowirePrimaryApplicationWorkingTests {
 
    @Autowired
    ApplicationContext ctx;
 
    @Test
-   void notWorking() {
-      log.info("[contextLoads] Initial Configuration ---------------------------------------------------------------------------------------------------------");
-      SomeHostDAS hostBean = ctx.getBean(SomeHostDAS.class);
-      assertInstanceOf(SomeHostDASImpl1.class, hostBean);
-      SomeBusinessService businessBean = ctx.getBean(SomeBusinessService.class);
-      log.info("[contextLoads] hostBean = {}, businessBean = {}", hostBean, businessBean);
-      businessBean.execute();
-      assertInstanceOf(SomeHostDASImpl1.class, businessBean.getHostDAS());
-
+   void working() {
       // Register a new SomeHostDAS implementation bean and set as primary
       BeanDefinition beanDefinition = BeanDefinitionBuilder.rootBeanDefinition(SomeHostDASImpl2.class).setPrimary(true).setAutowireMode(AutowireCapableBeanFactory.AUTOWIRE_BY_TYPE).getBeanDefinition();
       BeanDefinitionRegistry.class.cast(ctx).registerBeanDefinition("someHostDASImpl2", beanDefinition);
 
       log.info("[contextLoads] After Registering New Bean Definition ----------------------------------------------------------------------------------------\n{}", beanDefinition);
-      hostBean = ctx.getBean(SomeHostDAS.class);
+      SomeHostDAS hostBean = ctx.getBean(SomeHostDAS.class);
       assertInstanceOf(SomeHostDASImpl2.class, hostBean);
-      businessBean = ctx.getBean(SomeBusinessService.class);
+      SomeBusinessService businessBean = ctx.getBean(SomeBusinessService.class);
       log.info("[contextLoads] hostBean = {}, businessBean = {}", hostBean, businessBean);
       businessBean.execute();
       assertInstanceOf(SomeHostDASImpl2.class, businessBean.getHostDAS());
